7850ce5f696e6d1360a825dcebc07bfc68f895dc
Jeremy <Johnbovine@yahoo.com>
2019-03-21 19:55:08 -0500

    left wrapper styling, contact form

---
+++ src/components/contactForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react"
+import axios from "axios"
+
+const ContactForm = () => {
+  const [email, setEmail] = useState("test@test.com")
+  const [message, setMessage] = useState("test")
+  return (
+    <form
+      className="contact-form"
+      //placeholder for form submit TODO replace with netlify info
+      onSubmit={e => {
+        e.preventDefault()
+        axios
+          .post("/contact", {
+            email: email,
+            message: message,
+          })
+          .then(res => {
+            console.log(res)
+          })
+      }}
+    >
+      <div>
+        <label>Your Email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => {
+            setEmail(e.target.value)
+            console.log(email)
+          }}
+          required
+        />
+      </div>
+      <div>
+        <label>Your Message</label>
+        <textarea
+          name="message"
+          value={message}
+          onChange={e => {
+            setMessage(e.target.value)
+          }}
+          required
+        />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  )
+}
+
+export default ContactForm
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import ContactForm from "../components/contactForm"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -11,10 +10,47 @@ const IndexPage = () => (
     <div className="wrapper">
       <div className="wrapper__left-wrapper">
         <Image />
-        <h1>Jeremy Mitchell</h1>
+        <h1 className="header-firstname">
+          Jeremy <span className="header-lastname">Mitchell</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus
+          nullam eget felis eget nunc lobortis. Placerat vestibulum lectus
+          mauris ultrices eros in cursus turpis massa. Volutpat odio facilisis
+          mauris sit. Turpis egestas pretium aenean pharetra magna ac. Laoreet
+          non curabitur gravida arcu ac tortor dignissim convallis. Interdum
+          posuere lorem ipsum dolor sit. Faucibus purus in massa tempor nec
+          feugiat nisl pretium. Facilisis magna etiam tempor orci eu. At urna
+          condimentum mattis pellentesque id nibh. Eget felis eget nunc lobortis
+          mattis aliquam faucibus purus. Neque vitae tempus quam pellentesque
+          nec nam aliquam sem et. Tellus in metus vulputate eu scelerisque
+          felis. Pretium lectus quam id leo in vitae.
+        </p>
+        <p>
+          Amet justo donec enim diam vulputate ut pharetra. Morbi tempus iaculis
+          urna id volutpat lacus laoreet. Est ullamcorper eget nulla facilisi.
+          Eu tincidunt tortor aliquam nulla facilisi cras. Metus dictum at
+          tempor commodo. Elementum sagittis vitae et leo duis. Ultrices mi
+          tempus imperdiet nulla malesuada pellentesque elit. Lacus luctus
+          accumsan tortor posuere ac ut consequat. Dictum sit amet justo donec
+          enim diam vulputate. Nulla aliquet enim tortor at. Pulvinar etiam non
+          quam lacus suspendisse faucibus interdum. Ut tristique et egestas quis
+          ipsum suspendisse. Tellus cras adipiscing enim eu turpis egestas
+          pretium aenean. Non enim praesent elementum facilisis leo vel
+          fringilla est. A pellentesque sit amet porttitor eget. Tellus
+          elementum sagittis vitae et leo duis ut. Sit amet volutpat consequat
+          mauris nunc congue. Massa vitae tortor condimentum lacinia quis vel
+          eros.
+        </p>
+        <p>
+          Thanks for checking out my site. Email me at _______ at gmail dot com
+          or use the form below if you'd like to chat.
+        </p>
+        <ContactForm />
       </div>
       <div className="wrapper__right-wrapper">
-        Right Section
+        <h1 className="header-links">Links</h1>
         <div>Test</div>
         <div>Test</div>
         <div>Test</div>
