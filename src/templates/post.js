88ec80ebc79664ff97ca60adbcd5524817d62a9e
Jeremy <Johnbovine@yahoo.com>
2019-03-24 14:02:39 -0500

    post functionality working

---
+++ src/templates/post.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+
+export default function Template({ data }) {
+  //equivalent to const post = data.markdownRemark
+  const { markdownRemark: post } = data
+  return (
+    <div>
+      {/* take title from the frontmatter */}
+      <h1>{post.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  )
+}
+
+export const postQuery = graphql`
+  query PostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+      }
+    }
+  }
+`
